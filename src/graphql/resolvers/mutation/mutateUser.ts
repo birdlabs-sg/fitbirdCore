b1a36f89b208e20d5f6ff9b10e9ef396ce238f02
Dion <dionneo123@gmail.com>
2022-06-27 14:17:42 +0800

    feat: add support for more excercise fields

--- src/graphql/resolvers/mutation/mutateUser.ts
+++ src/graphql/resolvers/mutation/mutateUser.ts
@@ -2,18 +2,14 @@ import { onlyAdmin, onlyAuthenticated } from "../../../service/firebase_service"
 
 
 export const updateUser = async ( _:any, args: any, context:any ) => {
-    console.log("ss")
     onlyAuthenticated(context)
     const prisma = context.dataSources.prisma
-    // conduct check that the measurement object belongs to the user.
     const updatedUser = await prisma.user.update({
         where: {
             user_id : context.user.user_id
         },
         data:args,
     })
-
-    console.log(updatedUser);
 
     return {
         code: "200",
