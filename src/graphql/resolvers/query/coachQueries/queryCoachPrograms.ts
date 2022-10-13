6db86c4ac3bb3b632a6636e183aa99339212b2bc
DonovanVA <donovanipman@gmail.com>
2022-10-13 18:35:08 +0800

    Bug fix for #5

--- src/graphql/resolvers/query/coachQueries/queryCoachPrograms.ts
+++ src/graphql/resolvers/query/coachQueries/queryCoachPrograms.ts
@@ -12,7 +12,6 @@ export const coachProgramsResolver = async (
   const programs = await prisma.program.findMany({
     where: {
       coach_id: context.coach.coach_id,
-      is_active:true
     },
 
   });
--- src/graphql/resolvers/query/coachQueries/queryCoachUsers.ts
+++ src/graphql/resolvers/query/coachQueries/queryCoachUsers.ts
@@ -13,6 +13,7 @@ export const coachUsersQueryResolver = async (
   const users = await prisma.program.findMany({
     where: {
       coach_id: context.coach.coach_id,
+      is_active:true
     },
   });
 
